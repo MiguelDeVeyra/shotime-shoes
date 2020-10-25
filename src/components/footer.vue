aa7c5892d37404f714bd0575559000fa00048d93
MiguelDeVeyra <[work.deveyrajm@gmail.com]>
2020-10-25 12:46:44 +0800

    fixed footer link to linkedin

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -5,7 +5,7 @@
             <path d="M7.4 10.6L2.8 6L7.4 1.4L6 0L0 6L6 12L7.4 10.6ZM12.6 10.6L17.2 6L12.6 1.4L14 0L20 6L14 12L12.6 10.6V10.6Z" fill="black"/>
         </svg>
         <span > 
-            <a href="https:www.linkedin.com/in/miguel-de-veyra"> 
+            <a href="https://www.linkedin.com/in/miguel-de-veyra/"> 
                 © Miguel de Veyra 
             </a>
         </span>
